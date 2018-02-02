fb6898ead4fd6f17609a26ddab3c13bd644b9a42
Oliver Klee <oliver@phplist.com>
2018-02-02 13:24:37 +0100

    [FEATURE] Integration tests for the dev and test environment

---
+++ Tests/System/ApplicationBundle/PhpListApplicationBundleTest.php
@@ -0,0 +1,72 @@
+<?php
+declare(strict_types=1);
+
+namespace PhpList\WebFrontend\Tests\System\ApplicationBundle;
+
+use GuzzleHttp\Client;
+use PhpList\PhpList4\TestingSupport\Traits\SymfonyServerTrait;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Testcase.
+ *
+ * @author Oliver Klee <oliver@phplist.com>
+ */
+class PhpListApplicationBundleTest extends TestCase
+{
+    use SymfonyServerTrait;
+
+    /**
+     * @var Client
+     */
+    private $httpClient = null;
+
+    protected function setUp()
+    {
+        $this->httpClient = new Client();
+    }
+
+    protected function tearDown()
+    {
+        $this->stopSymfonyServer();
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function environmentDataProvider(): array
+    {
+        return [
+            'test' => ['test'],
+            'dev' => ['dev'],
+        ];
+    }
+
+    /**
+     * @test
+     * @param string $environment
+     * @dataProvider environmentDataProvider
+     */
+    public function homepageReturnsSuccess(string $environment)
+    {
+        $this->startSymfonyServer($environment);
+
+        $response = $this->httpClient->get('/', ['base_uri' => $this->getBaseUrl()]);
+
+        self::assertSame(200, $response->getStatusCode());
+    }
+
+    /**
+     * @test
+     * @param string $environment
+     * @dataProvider environmentDataProvider
+     */
+    public function homepageReturnsContent(string $environment)
+    {
+        $this->startSymfonyServer($environment);
+
+        $response = $this->httpClient->get('/', ['base_uri' => $this->getBaseUrl()]);
+
+        self::assertNotEmpty($response->getBody()->getContents());
+    }
+}

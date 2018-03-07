cfdd7558ceb5a84a0f9fc106784704093672a079
Oliver Klee <oliver@phplist.com>
2018-03-07 18:02:41 +0100

    [TASK] Use the new Core namespace

--- tests/System/ApplicationBundle/PhpListApplicationBundleTest.php
+++ tests/System/ApplicationBundle/PhpListApplicationBundleTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace PhpList\WebFrontend\Tests\System\ApplicationBundle;
 
 use GuzzleHttp\Client;
-use PhpList\PhpList4\TestingSupport\Traits\SymfonyServerTrait;
+use PhpList\Core\TestingSupport\Traits\SymfonyServerTrait;
 use PHPUnit\Framework\TestCase;
 
 /**
